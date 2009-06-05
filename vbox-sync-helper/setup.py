ca8f576f4f7a70501aab81aa52c2ed1dc37a7a25
Joachim Breitner <joachim.breitner@itomig.de>
2009-06-05 10:30:00 +0000

    Actually install manpage

--- vbox-sync-helper/setup.py
+++ vbox-sync-helper/setup.py
@@ -10,5 +10,5 @@ setup(name='vbox-sync-helper',
       packages=['itomig'],
       scripts=['vbox-sync', 'vbox-invoke', 'vbox-makecfg', 'vbox-dispose'],
       data_files=[('share/man/man1', ['vbox-invoke.1', 'vbox-makecfg.1']),
-                  ('share/man/man8', ['vbox-sync.8'])],
+                  ('share/man/man8', ['vbox-sync.8', 'vbox-dispose.8'])],
      )
