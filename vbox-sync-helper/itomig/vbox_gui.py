4ee5fcfc217d4005f96340e035e6fb6d72593492
Joachim Breitner <joachim.breitner@itomig.de>
2009-07-02 14:04:57 +0000

    Add vbox-sync-binary skeletton

---
+++ vbox-sync-helper/itomig/vbox_gui.py
@@ -0,0 +1,35 @@
+# vim:set et sw=4 encoding=utf-8:
+#
+# Module to handle the distribution of VBox VM images
+#
+# © 2009 Joachim Breitner <joachim.breitner@itomig.de>
+#
+# Licensed under the EUPL, Version 1.0 or – as soon they
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# you may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+# 
+# http://ec.europa.eu/idabc/eupl
+# 
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+
+from itomig.vbox import Logger
+
+__VERSION__ = "@VERSION@"
+
+class VBoxSyncAdminGui(object):
+    def __init__(self, config):
+        self.config = config
+        self.logger = Logger()
+
+    def main(self):
+        pass
+
