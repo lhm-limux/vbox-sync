f7365d212ac65598e4447d801b09d7d574d6185d
Joachim Breitner <mail@joachim-breitner.de>
2010-03-28 17:09:31 +0200

    Change maintainer name in setup.py

--- vbox-sync-helper/setup.py
+++ vbox-sync-helper/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(name='vbox-sync-helper',
       version='0.1',
-      author='Philipp Kern',
-      author_email='philipp.kern@itomig.de',
+      author='Joachim Breitner',
+      author_email='joachim.breitner@itomig.de',
       classifiers=[
         'License :: OSI Approved :: European Union Public License',
         'Programming Language :: Python'
