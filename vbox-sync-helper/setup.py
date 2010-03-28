7c0b629946b83a3d97784e7cabf7477ed7d7be76
Joachim Breitner <mail@joachim-breitner.de>
2010-03-28 17:09:08 +0200

    Ship additional files

--- vbox-sync-helper/setup.py
+++ vbox-sync-helper/setup.py
@@ -8,8 +8,8 @@ setup(name='vbox-sync-helper',
         'Programming Language :: Python'
         ],
       packages=['itomig'],
-      scripts=['vbox-sync', 'vbox-invoke', 'vbox-makecfg', 'vbox-dispose'],
-      data_files=[('share/man/man1', ['vbox-invoke.1', 'vbox-makecfg.1']),
+      scripts=['vbox-sync', 'vbox-invoke', 'vbox-makecfg', 'vbox-dispose', 'vbox-sync-admin'],
+      data_files=[('share/man/man1', ['vbox-invoke.1', 'vbox-makecfg.1', 'vbox-sync-admin.1']),
                   ('share/man/man8', ['vbox-sync.8', 'vbox-dispose.8'])],
       package_data={'itomig': ['vbox-sync-admin.glade']},
      )
